30a6ead3c31b14ad8306c76d84a44eb14fd37695
ahmed lallah <jj.93@oultook.com>
2021-02-27 13:58:57 +0100

    homepage overall styling + header and its components

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './homepage.style.scss';
+import Header from '../../components/header/header.component';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <Header/>
+    </div>
+);
+
+export default HomePage;
