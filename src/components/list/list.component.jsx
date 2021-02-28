3e5226117eb8aae5ea3b90412ac5f841739a5106
ahmed lallah <jj.93@oultook.com>
2021-02-28 18:56:45 +0100

    styling the list of recepies section

---
+++ src/components/list/list.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './list.style.scss';
+
+
+
+const List = () => (
+    <div className="list">
+        <div className="recepies">
+            <div className="img"></div>
+            <div className="info"></div>
+        </div>
+        <div className="pages"></div>
+    </div>
+);
+
+export default List;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './homepage.style.scss';
 import Header from '../../components/header/header.component';
+import List from '../../components/list/list.component';
 
 const HomePage = () => (
     <div className='homepage'>
         <Header/>
+        <List />
     </div>
 );
 
