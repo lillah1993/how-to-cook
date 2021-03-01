41f7bf38a72cb5a537731606eaf4544dd3ea66df
ahmed lallah <jj.93@oultook.com>
2021-03-01 22:56:41 +0100

    ceating layout of the recipe section + some cleaning

---
+++ src/components/recipe/recipe.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './recipe.style.scss';
+
+const Recipe = () => (
+    <div className="recipe">
+        <div className="recipe-img"></div>
+        <div className="instractions"></div>
+        <div className="gradients"></div>
+        <div className="go-to"></div>
+    </div>
+    
+);
+
+export default Recipe;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './homepage.style.scss';
 import Header from '../../components/header/header.component';
 import List from '../../components/list/list.component';
+import Recipe from '../../components/recipe/recipe.component';
 
 const HomePage = () => (
     <div className='homepage'>
         <Header/>
         <List />
+        <Recipe />
+
     </div>
 );
 
