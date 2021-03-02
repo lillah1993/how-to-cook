46934ce24cf52fc519b794e0d82eb98495f45335
ahmed lallah <jj.93@oultook.com>
2021-03-02 23:36:41 +0100

    styling the shopping list section

---
+++ src/components/shopping-ls/shopping-ls.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './shopping-ls.style.scss';
+
+const Shopping = () => (
+    <div className="shopping-ls">
+        <span>My shopping list</span>
+        <div className="main-ls"></div>
+    </div>
+);
+
+export default Shopping;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+
 import './homepage.style.scss';
+
 import Header from '../../components/header/header.component';
 import List from '../../components/list/list.component';
+import Shopping from '../../components/shopping-ls/shopping-ls.component';
 import Recipe from '../../components/recipe/recipe.component';
+
+
+
 
 const HomePage = () => (
     <div className='homepage'>
         <Header/>
         <List />
         <Recipe />
-
+        <Shopping />
     </div>
 );
 
